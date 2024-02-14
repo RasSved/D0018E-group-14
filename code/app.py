dc93bfe6e451e565dce6b5f9821d172916b2dc66
RasSved <150154317+RasSved@users.noreply.github.com>
2024-02-14 11:34:48 +0100

    Update app.py

--- code/app.py
+++ code/app.py
@@ -1,11 +1,40 @@
 from flask import Flask, render_template 
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+import pymysql
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://20010615:sumsar1928@127.0.0.1/db20010615' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app) 
+pymysql.connect(db='db20010615', user='20010615', passwd='sumsar1928', host='127.0.0.1', port=3306)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    bio = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Student {self.firstname}>'
 
 
 @app.route('/')
-def index():
-    return render_template('start.html', title = "start" )
+def testdb():
+    try:
+        db.session.query(db.text('1')).from_statement(db.text('SELECT 1')).all()
+        return '<h1>It works.</h1>'
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
 
 @app.route('/customer.html')
 def customer():
